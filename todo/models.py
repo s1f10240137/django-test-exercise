b3d3fabd814b98fcda311b5a3ca95d4e2823c4ea
1F10240137 <s1F102401371@iniad.org>
2025-06-26 14:37:49 +0900

    Taskモデルにメソッドを追加

--- todo/models.py
+++ todo/models.py
@@ -7,3 +7,8 @@ class Task(models.Model):
     completed = models.BooleanField(default=False) #boolean型のカラム(デフォルトはFalse、完了しているか)
     posted_at = models.DateTimeField(default=timezone.now) #datetime型のカラム(デフォルトは現在時刻、登録日)
     due_at = models.DateTimeField(null=True, blank=True) #datetime型のカラム(nullを可とする、締切)
+
+    def is_overdue(self, dt):
+        if self.due_at is None:
+            return False
+        return self.due_at < dt
