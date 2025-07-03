0aa0f1864b471b17cd1a1ceb9dfa98c0a0504c70
1F10240137 <s1F102401371@iniad.org>
2025-07-03 14:07:21 +0900

    コードを整形

--- todo/tests.py
+++ todo/tests.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
+
 # Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample(self):
@@ -12,7 +13,7 @@ class SampleTestCase(TestCase):
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30,
-23, 59, 59)) #datatimeに適切なタイムゾーンの設定
+23, 59, 59))
         task = Task(title='task1', due_at=due)
         task.save()
 
@@ -45,7 +46,7 @@ class TaskModelTestCase(TestCase):
         task.save()
 
         self.assertTrue(task.is_overdue(current))
-    
+
     def test_is_overdue_none(self):
         current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
         task = Task(title='task1', due_at=None)
@@ -53,14 +54,15 @@ class TaskModelTestCase(TestCase):
 
         self.assertFalse(task.is_overdue(current))
 
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
-        response = client.get('/') #GETリクエストを送信
+        response = client.get('/')
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(len(response.context['tasks']), 0) # 初期状態ではタスクがないことを確認
+        self.assertEqual(len(response.context['tasks']), 0)
 
     def test_index_post(self):
         client = Client()
@@ -81,7 +83,7 @@ class TodoViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
-        self.assertEqual(response.context['tasks'] [0], task2) # 最新の投稿が最初に来ることを確認
+        self.assertEqual(response.context['tasks'] [0], task2)
         self.assertEqual(response.context['tasks'][1], task1)
 
     def test_index_get_order_due(self):
--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
 
+
 # Create your views here.
 def index(request):
     if request.method == 'POST':
@@ -18,4 +19,4 @@ def index(request):
     context = {
         'tasks': tasks
     }
-    return render(request, 'todo/index.html', context)            
+    return render(request, 'todo/index.html', context)
