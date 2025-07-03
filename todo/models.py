dfcabfff0f114e5a1f600478f8be5e257890d5b3
1F10240137 <s1F102401371@iniad.org>
2025-07-03 13:55:01 +0900

    model.pyのエラーを修正

--- todo/models.py
+++ todo/models.py
@@ -3,10 +3,11 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length=100) #varchar型のカラム(最大100文字、タイトル)
-    completed = models.BooleanField(default=False) #boolean型のカラム(デフォルトはFalse、完了しているか)
-    posted_at = models.DateTimeField(default=timezone.now) #datetime型のカラム(デフォルトは現在時刻、登録日)
-    due_at = models.DateTimeField(null=True, blank=True) #datetime型のカラム(nullを可とする、締切)
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(default=timezone.now)
+    due_at = models.DateTimeField(null=True, blank=True)
+
 
     def is_overdue(self, dt):
         if self.due_at is None:
--- todo/tests.py
+++ todo/tests.py
@@ -8,6 +8,7 @@ class SampleTestCase(TestCase):
     def test_sample(self):
         self.assertEqual(1 + 2, 3)
 
+
 class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30,
